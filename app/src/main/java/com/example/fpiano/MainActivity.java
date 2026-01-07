09d85cbce4bc5351da670ab9b4442939c9f9c60f
JEEL <jeelp441@gmail.com>
2026-01-07 13:55:16 +0530

    import ted soundpool and create the variables

--- app/src/main/java/com/example/fpiano/MainActivity.java
+++ app/src/main/java/com/example/fpiano/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.fpiano;
 
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    SoundPool sp;
+
+    // RECORDING
+    boolean isRecording = false;
+    ArrayList<NoteEvent> recordedNotes = new ArrayList<>();
+
+    // WHITE KEYS
+    int c, d, e, f, g, a, b, c2, d2, e2, f2, g2, a2, b2;
+
+    // BLACK KEYS
+    int cs, ds, fs, gs, as, cs2, ds2, fs2, gs2, as2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
     }
 }
