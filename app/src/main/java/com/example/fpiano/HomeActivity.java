5282d6a767011552fa56d26abb0cee90aea6ca8e
jeel <jeelp441@gmail.com>
2026-01-18 13:13:30 +0530

    intent added to piano screen

--- app/src/main/java/com/example/fpiano/HomeActivity.java
+++ app/src/main/java/com/example/fpiano/HomeActivity.java
@@ -1,20 +1,34 @@
 package com.example.fpiano;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.fpiano.R;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        // Fullscreen safe
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
         setContentView(R.layout.activity_home);
 
+        Button btnPlay = findViewById(R.id.btnPlay);
+
+        btnPlay.setOnClickListener(v -> {
+            startActivity(
+                    new Intent(HomeActivity.this, MainActivity.class)
+            );
+        });
     }
 }
